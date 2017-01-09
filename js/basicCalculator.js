dc318bc355e1fe5dc76b285138d747077e3182b2
Kory Skarbek and Melvin Gruschow <kory@>
2017-01-09 09:33:56 -0800

    Created basic page skeleton for index made all folders initiated files

---
+++ js/basicCalculator.js
@@ -0,0 +1,10 @@
+var add = function(number1, number2) {
+	return number1 + number2;
+};
+
+//alert(add(10, 5))
+
+var numb1 = parseInt(prompt("Enter a number:"));
+var numb2 = parseInt(prompt("Enter another number:"));
+
+alert(add(numb1, numb2));
