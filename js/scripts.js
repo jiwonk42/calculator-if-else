64f8c1f156a0ed54645c07e9c622e45b0b4d94e9
jiwon kang and matt caswell <matt@>
2017-01-12 16:10:59 -0800

    complete our calculator using if and else statements

--- js/scripts.js
+++ js/scripts.js
@@ -18,40 +18,21 @@ var divide = function(number1, number2) {
 
 // Everything below this line is user interface (or front-end) logic:
 $(document).ready(function(){
-	$("form#add").submit(function(event){
+	$("form#calculate").submit(function(event){
 		event.preventDefault();
-		var numb1 = parseInt($("#add1").val());
-		var numb2 = parseInt($("#add2").val());
-		var resultAdd = add(numb1, numb2);
-		$("button#add").click(function() {
-			$("#outputAdd").text(resultAdd);
-		});
-	});
-	$("form#add").submit(function(event){
-		event.preventDefault();
-		var numb1 = parseInt($("#add1").val());
-		var numb2 = parseInt($("#add2").val());
-		var resultSub = subtract(numb1, numb2);
-		$("button#sub").click(function() {
-			$("#outputSub").text(resultSub);
-		});
-	});
-	$("form#add").submit(function(event){
-		event.preventDefault();
-		var numb1 = parseInt($("#add1").val());
-		var numb2 = parseInt($("#add2").val());
-		var resultMultiply = multiply(numb1, numb2);
-		$("button#multiply").click(function() {
-			$("#outputMultiply").text(resultMultiply);
-		});
-	});
-	$("form#add").submit(function(event){
-		event.preventDefault();
-		var numb1 = parseInt($("#add1").val());
-		var numb2 = parseInt($("#add2").val());
-		var resultDivide = divide(numb1, numb2);
-		$("button#divide").click(function() {
-			$("#outputDivide").text(resultDivide);
-		});
+		var number1 = parseInt($("#number1").val());
+		var number2 = parseInt($("#number2").val());
+		var operator = $("input:radio[name=operator]:checked").val();
+    var result;
+    if (operator === "add") {
+      result = add(number1, number2);
+    } else if (operator === "subtract") {
+      result = subtract(number1, number2);
+    } else if (operator === "multiply") {
+      result = multiply(number1, number2);
+    } else if (operator === "divide") {
+      result = divide(number1, number2);
+    }
+    $("#output").text(result);
 	});
 });
